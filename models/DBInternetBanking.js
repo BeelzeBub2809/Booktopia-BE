49e5e7de586836e3be039ef3555c93aada519dd6
BeelzeBub2809 <nguyenanhtuan28092003@gmail.com>
2024-10-22 13:23:32 +0700

    Implement sign up

--- models/DBInternetBanking.js
+++ models/DBInternetBanking.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const internetBankingSchema = new mongoose.Schema({
   bank: { type: String },
-  bankAccountNumber: { type: String }
+  bankAccountNumber: { type: String },
+  customerID: { type: mongoose.Schema.Types.ObjectId, ref: 'DBCustomer' }
 });
 
 const DBInternetBanking = mongoose.model('DBInternetBanking', internetBankingSchema);
