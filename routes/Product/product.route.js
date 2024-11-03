4664d275cb6333a18d15f1fd6ab07102986d8dff
BeelzeBub2809 <nguyenanhtuan28092003@gmail.com>
2024-11-03 16:15:00 +0700

    Implement add multiple product image in product manage

--- routes/Product/product.route.js
+++ routes/Product/product.route.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const {ProductController} = require('../../controllers');
 const AuthMiddlewares = require('../../middlewares/auth.middleware');
+const Extension = require('../../extensions/uploadImage');
 
 const productRoute = express.Router();
 
 productRoute.get('/search', ProductController.getProductByName);
 productRoute.get('/', ProductController.getProduct);
 productRoute.get('/:id', ProductController.getProductById);
-productRoute.post('/', ProductController.createProduct);
+productRoute.post('/', [Extension.upload.array('images', 5)],ProductController.createProduct);
 productRoute.put('/:id', ProductController.updateProduct);
 productRoute.delete('/:id', ProductController.deleteProduct);
 productRoute.post('/all', ProductController.getAllProductsBySales);
