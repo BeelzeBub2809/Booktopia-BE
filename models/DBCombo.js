21d9e412f4e9a868d29aaaa59416caae9e218064
TungDuongPham <duongpthe173109@fpt.edu.vn>
2024-11-05 01:24:12 +0700

    fix search productbyname

--- models/DBCombo.js
+++ models/DBCombo.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const comboSchema = new mongoose.Schema({
   name: { type: String },
   productId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'DBProduct' }],
-  price: { type: mongoose.Types.Decimal128 },
-  discount: { type: mongoose.Types.Decimal128 },
+  price: { type: Number, required: true },
+  discount: { type: Number},
   quantity: { type: Number },
   status: { type: String },
   image: [{ type: String }],
